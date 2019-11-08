c991520655d054fe8a848f042d186fd1bebafed2
jonathan <jonathan.gerst@hs-kl.de>
2019-11-08 19:52:18 +0100

    zuzu

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -27,7 +27,7 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
-
+//Test Kommentar 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
